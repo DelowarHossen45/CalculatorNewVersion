3588e6035f79153198c6405fb23d489fe87f90cf
Delowar Hossen Apple <applecse17@gmail.com>
2019-09-29 11:45:03 +0600

    text view complete

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,16 +1,29 @@
 package com.example.calculator;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
-
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textView1, textView2;
+    Button zero, one, two, three, four, five, six, seven, eight, nine, divided, multiply, minus,
+            pluse, equal, dot, clear,root,power,parcend;
+    ImageButton back;
+    double valueOne=Double.NaN, valueTwo=Double.NaN,vpower;
+    int action;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView1 = (TextView) findViewById(R.id.content);
+        textView2 = (TextView) findViewById(R.id.result);
+
     }
 }
